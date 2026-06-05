74343fdeb4ddfb26bbe530c25a598e9e94b11a7e
Brijeshiam <brijesh11007@gmail.com>
2026-06-05 10:52:03 +0000

    changes

--- variables.tf
+++ variables.tf
@@ -2,6 +2,6 @@ variable "region" {
   description = "the name of the region in  which you are going to store the  data"
 }
 variable "cidr" {
-  description = "the network base addrress"
+  description = "the network base addrress ee"
 
 }
